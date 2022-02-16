67f7058a4c7a697602c2cd4afc76ceeb1f71a058
Nishan6981 <nishan6981@gmail.com>
2022-02-16 22:18:56 +0600

    new code

---
+++ js/custome.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').owlCarousel ({
+        items:1,
+        autoplay:true,
+        autoplayTimeout: 5000,
+        loop:true,
+        smartSpeed: 500,
+    })
+})
